e6259b65db72c397bdcd0930db8e0ca82b3323f9
iam-rasulov <mirjalolrasulov002@gmail.com>
2022-04-02 17:42:01 +0500

    add title

--- js/main.js
+++ js/main.js
@@ -2,48 +2,46 @@ const elList = document.querySelector(".list");
 const newList = document.querySelector(".new-list");
 const search = document.querySelector(".search");
 
-
 const splicedList = [];
 
 
 function renderGenes(arr , element){
-
+  
   var renderGeners = [];
-
+  
   arr.forEach((film) => {
-
+    
     film.genres.forEach(genre => {
       if(!renderGeners.includes(genre)){
         renderGeners.push(genre)
       }
     })
   })
-
+  
   renderGeners.forEach(genre => {
     const newOption = document.createElement("option");
     newOption.value = genre;
     newOption.textContent = genre;
     element.appendChild(newOption);
   })
-
+  
 }
 
 
 function renderFilms(arr, element){
   element.innerHTML = "";
-
+  
   arr.forEach(film =>{
-    var newItem = document.createElement("li");
-    var newImg = document.createElement("img");
-    var newBox = document.createElement("div");
-    var newHeading = document.createElement("h3");
-    var newText = document.createElement("p");
-    var newBtn = document.createElement("button");
-
-
+    const newItem = document.createElement("li");
+    const newImg = document.createElement("img");
+    const newBox = document.createElement("div");
+    const newHeading = document.createElement("h3");
+    const newText = document.createElement("p");
+    const newBtn = document.createElement("button");
+    
     newHeading.textContent = film.title;
     newText.textContent = film.overview.split(" ").slice(0 ,10).join(" ") + "...";
-
+    
     newItem.setAttribute("class", "list__item");
     newImg.setAttribute("src", film.poster);
     newBox.setAttribute("class", "item__box")
@@ -54,101 +52,79 @@ function renderFilms(arr, element){
     newBtn.setAttribute("class", "list__btn");
     newBtn.textContent = "favorite"
     newBtn.dataset.btnId = film.id;
-
     
     newItem.appendChild(newImg);
     newItem.appendChild(newBox);
     newBox.appendChild(newHeading);
     newBox.appendChild(newText);
     newBox.appendChild(newBtn);
-
+    
     element.appendChild(newItem);
-
   })
-
+  
 }
 
 
-function renderNames(arr, element){
+function renderBookmark(arr, element){
   element.innerHTML = ""
-
+  
   arr.forEach(n => {
-
+    
     let newItem = document.createElement("li");
     let newBtn = document.createElement("button");
-
     
     newItem.textContent = n.title;
     newBtn.textContent = "remove";
-
+    
     newItem.setAttribute("class", "new-item");
     newBtn.setAttribute("class", "new-btn");
     newBtn.dataset.btnId = n.id;
-
     
     element.appendChild(newItem);
     newItem.appendChild(newBtn);
-    
   })
 }
 
 
-  elList.addEventListener("click", evt =>{
-    if(evt.target.matches(".list__btn")){
-
-      let btnId = evt.target.dataset.btnId;
-  
-      let findIndexArr = films.findIndex(film => film.id == btnId);
-
-      splicedList.push.findIndexArr;
-
-      if(!splicedList.includes(findIndexArr)){
-        splicedList.push(findIndexArr);
-      }
-  
-    }
+elList.addEventListener("click", evt =>{
+  if(evt.target.matches(".list__btn")){
     
-    renderNames(splicedList, newList);
+    let btnId = evt.target.dataset.btnId;
     
-  });
-  
-
-  newList.addEventListener("click", evt =>{
-    if(evt.target.matches(".new-btn")){
-
-      const btnId = evt.target.dataset.btnId;
-  
-      const findIndexArr = films.findIndex(n => n.id == btnId);
-  
-      splicedList.splice(findIndexArr, 1);
-
-      renderNames(splicedList, newList);
-      
+    let findIndexArr = films.find(film => film.id == btnId);
+    
+    splicedList.push.findIndexArr;
+    
+    if(!splicedList.includes(findIndexArr)){
+      splicedList.push(findIndexArr);
     }
-  });
+  }
+  renderBookmark(splicedList, newList);
+});
+
+
+newList.addEventListener("click", evt =>{
+  if(evt.target.matches(".new-btn")){
+    
+    const btnId = evt.target.dataset.btnId;
+    
+    const findIndexArr = films.findIndex(n => n.id == btnId);
+    
+    splicedList.splice(findIndexArr, 1);
+    
+    renderBookmark(splicedList, newList);
+  }
+});
+
 
 form.addEventListener("submit", evt =>{
   evt.preventDefault();
-
-  const selectVal = select.value;
-
-  let filterFilms = selectVal == "all" ? films : films.filter(element => element.genres.includes(selectVal))  ;
-
-  // if(selectVal === "all"){
-  //   filterFilms = films;
-  // }else{
-  //   filterFilms = films.filter(element => element.genres.includes(selectVal));
-  // }
-
-  renderFilms(filterFilms, elList);
-
   
-  // const searchVal = search.value;
-
-  // let filterSearchs = searchVal == "all" ? films : films.filter(element => element.title.includes(searchVal))  ;
-
-  // renderFilms(filterSearchs, elList)
-
+  const selectVal = select.value;
+  
+  let filterFilms = selectVal == "all" ? films : films.filter(element => element.genres.includes(selectVal))  ;
+  
+  renderFilms(filterFilms, elList);
 })
 
 renderFilms(films, elList);
